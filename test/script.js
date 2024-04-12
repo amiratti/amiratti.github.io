ac8574cd7ab16c8bed3b77fea787831b99d1f062
amiratti <alyamiratti@gmail.com>
2024-04-12 17:37:34 -0400

    test

--- test/script.js
+++ test/script.js
@@ -1,6 +1,17 @@
 
-
   let myStitch = document.getElementsByClassName("stitch");
-  function changeColor() {
-    myStitch.classList.toggle("fill");
+  console.log(myStitch);
+
+  function fill1aSquare() {
+   
+    let _1aStitches = document.getElementsByClassName("_1a");
+    console.log(_1aStitches);
+    for (let _1aStitch of _1aStitches){
+     // _1aStitch.style.backgroundColor ="red";
+     _1aStitch.classList.toggle("fill");
+    }
   }
+
+
+  //let  = document.getElementById("color-picker");
+  //function 
