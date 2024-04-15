d33ef54c2427aa2d24d3dd58e4f4ea43220fde0b
amiratti <alyamiratti@gmail.com>
2024-04-15 01:16:45 -0400

    knit!

---
+++ test2/script.js
@@ -0,0 +1,216 @@
+
+let myStitch = document.getElementsByClassName("canvas-stitch");
+console.log(myStitch);
+
+function fill1aSquare() {
+ 
+  let _1aStitches = document.getElementsByClassName("_1a");
+  console.log(_1aStitches);
+  for (let _1aStitch of _1aStitches){
+   _1aStitch.classList.toggle("fill");
+  }
+}
+
+function fill1bSquare() {
+ 
+    let _1bStitches = document.getElementsByClassName("_1b");
+    console.log(_1bStitches);
+    for (let _1bStitch of _1bStitches){
+     _1bStitch.classList.toggle("fill");
+    }
+  }
+
+  function fill1cSquare() {
+ 
+    let _1cStitches = document.getElementsByClassName("_1c");
+    console.log(_1cStitches);
+    for (let _1cStitch of _1cStitches){
+     _1cStitch.classList.toggle("fill");
+    }
+  }
+  function fill1dSquare() {
+ 
+    let _1dStitches = document.getElementsByClassName("_1d");
+    console.log(_1dStitches);
+    for (let _1dStitch of _1dStitches){
+     _1dStitch.classList.toggle("fill");
+    }
+  }
+
+  function fill1eSquare() {
+ 
+    let _1eStitches = document.getElementsByClassName("_1e");
+    console.log(_1eStitches);
+    for (let _1eStitch of _1eStitches){
+     _1eStitch.classList.toggle("fill");
+    }
+  }
+
+  function fill2aSquare() {
+ 
+    let _2aStitches = document.getElementsByClassName("_2a");
+    console.log(_2aStitches);
+    for (let _2aStitch of _2aStitches){
+     _2aStitch.classList.toggle("fill");
+    }
+  }
+
+  function fill2bSquare() {
+ 
+    let _2bStitches = document.getElementsByClassName("_2b");
+    console.log(_2bStitches);
+    for (let _2bStitch of _2bStitches){
+     _2bStitch.classList.toggle("fill");
+    }
+  }
+
+  function fill2cSquare() {
+ 
+    let _2cStitches = document.getElementsByClassName("_2c");
+    console.log(_2cStitches);
+    for (let _2cStitch of _2cStitches){
+     _2cStitch.classList.toggle("fill");
+    }
+  }
+
+function fill2dSquare() {
+ 
+    let _2dStitches = document.getElementsByClassName("_2d");
+    console.log(_2dStitches);
+    for (let _2dStitch of _2dStitches){
+     _2dStitch.classList.toggle("fill");
+    }
+  }
+function fill2eSquare() {
+ 
+    let _2eStitches = document.getElementsByClassName("_2e");
+    console.log(_2eStitches);
+    for (let _2eStitch of _2eStitches){
+     _2eStitch.classList.toggle("fill");
+    }
+  }
+
+  function fill3aSquare() {
+ 
+    let _3aStitches = document.getElementsByClassName("_3a");
+    console.log(_3aStitches);
+    for (let _3aStitch of _3aStitches){
+     _3aStitch.classList.toggle("fill");
+    }
+  }
+
+  function fill3bSquare() {
+ 
+    let _3bStitches = document.getElementsByClassName("_3b");
+    console.log(_3bStitches);
+    for (let _3bStitch of _3bStitches){
+     _3bStitch.classList.toggle("fill");
+    }
+  }
+
+function fill3cSquare() {
+ 
+    let _3cStitches = document.getElementsByClassName("_3c");
+    console.log(_3cStitches);
+    for (let _3cStitch of _3cStitches){
+     _3cStitch.classList.toggle("fill");
+    }
+  }
+
+  function fill3dSquare() {
+ 
+    let _3dStitches = document.getElementsByClassName("_3d");
+    console.log(_3dStitches);
+    for (let _3dStitch of _3dStitches){
+     _3dStitch.classList.toggle("fill");
+    }
+  }
+  function fill3eSquare() {
+ 
+    let _3eStitches = document.getElementsByClassName("_3e");
+    console.log(_3eStitches);
+    for (let _3eStitch of _3eStitches){
+     _3eStitch.classList.toggle("fill");
+    }
+  }
+  function fill4aSquare() {
+ 
+    let _4aStitches = document.getElementsByClassName("_4a");
+    console.log(_4aStitches);
+    for (let _4aStitch of _4aStitches){
+     _4aStitch.classList.toggle("fill");
+    }
+  }
+  function fill4bSquare() {
+ 
+    let _4bStitches = document.getElementsByClassName("_4b");
+    console.log(_4bStitches);
+    for (let _4bStitch of _4bStitches){
+     _4bStitch.classList.toggle("fill");
+    }
+  }
+  function fill4cSquare() {
+ 
+    let _4cStitches = document.getElementsByClassName("_4c");
+    console.log(_4cStitches);
+    for (let _4cStitch of _4cStitches){
+     _4cStitch.classList.toggle("fill");
+    }
+  }
+
+  function fill4dSquare() {
+ 
+    let _4dStitches = document.getElementsByClassName("_4d");
+    console.log(_4dStitches);
+    for (let _4dStitch of _4dStitches){
+     _4dStitch.classList.toggle("fill");
+    }
+  }
+  function fill4eSquare() {
+ 
+    let _4eStitches = document.getElementsByClassName("_4e");
+    console.log(_4eStitches);
+    for (let _4eStitch of _4eStitches){
+     _4eStitch.classList.toggle("fill");
+    }
+  }
+  function fill5aSquare() {
+ 
+    let _5aStitches = document.getElementsByClassName("_5a");
+    console.log(_5aStitches);
+    for (let _5aStitch of _5aStitches){
+     _5aStitch.classList.toggle("fill");
+    }
+  }
+  function fill5bSquare() {
+ 
+    let _5bStitches = document.getElementsByClassName("_5b");
+    console.log(_5bStitches);
+    for (let _5bStitch of _5bStitches){
+     _5bStitch.classList.toggle("fill");
+    }
+  }
+  function fill5cSquare() {
+ 
+    let _5cStitches = document.getElementsByClassName("_5c");
+    console.log(_5cStitches);
+    for (let _5cStitch of _5cStitches){
+     _5cStitch.classList.toggle("fill");
+    }
+  }
+  function fill5dSquare() {
+ 
+    let _5dStitches = document.getElementsByClassName("_5d");
+    console.log(_5dStitches);
+    for (let _5dStitch of _5dStitches){
+     _5dStitch.classList.toggle("fill");
+    }
+  }
+  function fill5eSquare() {
+ 
+    let _5eStitches = document.getElementsByClassName("_5e");
+    console.log(_5eStitches);
+    for (let _5eStitch of _5eStitches){
+     _5eStitch.classList.toggle("fill");
+    }
+  }
