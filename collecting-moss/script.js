e4949f884305d848d343068f333c076c18cbcc97
amiratti <alyamiratti@gmail.com>
2024-04-29 01:07:09 -0400

    blur adjust

--- collecting-moss/script.js
+++ collecting-moss/script.js
@@ -1,6 +1,6 @@
 function blurMossText(){
     let mossText = document.getElementById("moss-text");
-    mossText.style.textShadow = "0 0 " + Math.random() * 15 + "px forestGreen";
+    mossText.style.textShadow = "0 0 " + Math.random() * 12 + "px forestGreen";
    
 }
 setInterval(blurMossText, 1000);
