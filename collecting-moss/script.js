afb6302a574466e6f1d137766eebacd0881f3d24
amiratti <alyamiratti@gmail.com>
2024-04-30 19:19:51 -0400

    append moss

--- collecting-moss/script.js
+++ collecting-moss/script.js
@@ -1,8 +1,20 @@
 function blurMossText(){
     let mossText = document.getElementById("moss-text");
-    mossText.style.textShadow = "0 0 " + Math.random() * 15 + "px forestGreen";
+    mossText.style.textShadow = "0 0 " + Math.random() * 17 + "px forestGreen";
    
 }
-setInterval(blurMossText, 1000);
-let thisSecond = today.getSeconds();
+setInterval(blurMossText, 900);
+
+
+function createMossball(){
+    const mossBall = document.createElement("moss-ball");
+    mossBall.classList.add('moss-ball');
+    mossBall.innerText = "O";
+    document.body.appendChild(mossBall);
+}
+setInterval(createMossball, 1500);
+
+function mouseOver() {
+    document.getElementsByClassName("moss-ball").style.backgroundColor = "white";
+  }
 
