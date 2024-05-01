505872b70ce291344e21f74804892cf30582146a
amiratti <alyamiratti@gmail.com>
2024-05-01 15:32:17 -0400

    random blur

--- collecting-moss/script.js
+++ collecting-moss/script.js
@@ -3,7 +3,9 @@ function blurMossText(){
     mossText.style.textShadow = "0 0 " + Math.random() * 17 + "px forestGreen";
    
 }
+
 setInterval(blurMossText, 1000);
+
 
 
 function createMossball(){
@@ -11,14 +13,26 @@ function createMossball(){
     mossBall.classList.add('moss-ball');
     document.body.appendChild(mossBall); 
     mossBall.style.left = Math.random() * 100 + "vw";
-    mossBall.style.top = Math.random() * 100 + "vh";
-    mossBall.style.filter = "blur(" + Math.random() * 25 +  "px)";
-    mossBall.style.transition = 3 + "s";
-
-    mossBall.onmouseover = function() {
-        mossBall.style.display = "none";
+    mossBall.style.top = Math.random() * 100 + "vh";  
+ 
+    function opacityMossBall(){
+        var randomOpacity =  Math.floor(Math.random() * 100) + 70;
+        mossBall.style.opacity = (randomOpacity); 
+    } 
+    setTimeout(opacityMossBall, 300);
+ 
+    var randomSize = Math.floor(Math.random() * 100) + 50;
+    mossBall.style.width = (randomSize) +"px";
+    mossBall.style.height = (randomSize) +"px";
+    function blurMossBall(){
+        var randomBlur = Math.floor(Math.random() * 17) + 8;
+        mossBall.style.filter = "blur(" + (randomBlur) +  "px)"; 
     }
-
+    setInterval(blurMossBall, 1500);
+    mossBall.style.transition = 1.5 + "s";
+    mossBall.onmouseover = function() {
+        mossBall.style.opacity = "0";
+    }
 }
 setInterval(createMossball, 2000);
 
